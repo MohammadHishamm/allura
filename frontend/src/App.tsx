0d916d7077d6549835fbf86e89604d83e9fcc090
ZAYATY-260 <zeiadabdelnasserelzayaty@gmail.com>
2025-09-05 21:46:09 +0300

    fixing issues

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CardNav, { items } from "./components/CardNav";
 import LightRays from "./components/background";
@@ -6,7 +5,7 @@ import TargetCursor from "./components/Cursor";
 import PricingPlans from "./components/PricingPlans";
 import Footer from "./components/Footer";
 import ModelViewer from "./components/ModelViewer";
-
+import Admin from "./components/admin";
 const App = () => {
   const logo = "/path-to-logo.png";
 
@@ -49,9 +48,9 @@ const App = () => {
           <Route
             path="/"
             element={
-              <main className="relative z-20 mt-8">
+              <main className="relative z-20  md:mt-8">
                 {/* Hero Section */}
-                <section className="relative min-h-screen flex flex-col md:flex-row justify-center items-center text-white px-3 md:px-20">
+                <section className="relative min-h-screen flex flex-col md:flex-row justify-center items-center text-white  md:px-20">
                   {/* Text Left */}
                   <div className="md:w-1/2 flex flex-col justify-center items-start space-y-6 z-20 ms-4 ms-6">
                     <h1 className="text-6xl font-bold leading-tight">
@@ -64,19 +63,17 @@ const App = () => {
                   </div>
 
                   {/* 3D Model Right */}
-                  <div className="md:w-1/2 w-full  md:mt-0 ">
+                  <div className="md:w-1/2 w-full md:mt-0 md:flex hidden">
                     <ModelViewer url="/models/gaming_desktop_pc_blend_file/scene.gltf" />
                   </div>
 
                   {/* Scroll Mouse Animation */}
-                  <div className="absolute bottom-25 flex justify-center w-full z-20">
+                  <div className="absolute bottom-20  justify-center w-full z-20 md:flex hidden">
                     <div className="w-[30px] h-[50px] border-2 border-white rounded-full flex justify-center items-start p-[4px]">
                       <div className="w-2 h-2 bg-white rounded-full animate-scroll"></div>
                     </div>
                   </div>
                 </section>
-
-
                 {/* Pricing Plans Section */}
                 <section className="py-20 bg-gray-900">
                   <PricingPlans />
@@ -84,11 +81,16 @@ const App = () => {
               </main>
             }
           />
+          <Route path="/admin297_2" element={<Admin />} />
+
         </Routes>
+
+
       </BrowserRouter>
 
       {/* Footer */}
       <Footer />
+
     </div>
   );
 };
